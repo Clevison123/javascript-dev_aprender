c4e92d666323d9adec74c6e4493892b9bba873c6
Clevison123 <cdefensor467@gmail.com>
2023-04-16 16:05:28 -0300

    Contador de Asteríscos (Mini-Projeto 9)

---
+++ mini-projeto 09/contador-de-asteriscos.js
@@ -0,0 +1,23 @@
+//Contador de Asteríscos (Mini-Projeto 9)
+
+//Criar uma função que exibe a quantidade de * que aquela linha linha possui
+
+exibirAsteriscos(5)
+
+function exibirAsteriscos(linhas) {
+//    let padrao = ''
+//    for(let linha = 1; linha <= linhas; linha++) {
+//        padrao += '*'
+//        console.log(padrao)
+//    }
+
+//Segundo jeito de se fazer 
+    for(let linha = 1; linha <= linhas; linha++){
+        let padrao = ''
+        for(let i = 0; i < linha; i++){
+            padrao += '*'
+            console.log(padrao)
+        }
+    }
+
+}
