47b022ee3fa6bd7d2804d38bde7e5e738f09c466
Clevison123 <cdefensor467@gmail.com>
2023-04-17 12:44:38 -0300

    aula sobre factory functions

---
+++ aulas/factory-funtions.js
@@ -0,0 +1,11 @@
+const celular = {
+    marcaCelular : 'ASUS',
+    tamanhoTela : {
+        vertical : 155,
+        horizontal : 75
+    }
+    capacidadeBateria: 5000,
+    ligar function() {
+        console.log("Fazendo ligação...")
+    }
+}
