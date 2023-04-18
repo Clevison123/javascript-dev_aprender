37306ce1347f98231ee1e1a2b844f799fd822096
Clevison123 <cdefensor467@gmail.com>
2023-04-18 13:00:38 -0300

    atualizei o código

--- aulas/factory-funtions.js
+++ aulas/factory-funtions.js
@@ -1,11 +1,27 @@
+
 const celular = {
     marcaCelular : 'ASUS',
     tamanhoTela : {
         vertical : 155,
         horizontal : 75
-    }
+    },
     capacidadeBateria: 5000,
-    ligar function() {
+    ligar : function() {
         console.log("Fazendo ligação...")
     }
 }
+
+
+function criarCelular(marcaCelular, tamanhoTela,capacidadeBateria){
+    return {
+        marcaCelular,
+        tamanhoTela,
+        capacidadeBateria,
+        ligar() {
+            console.log("Fazendo ligação...")
+        }
+    }
+}
+
+const celular1 = criarCelular('zenfone', 5.5, 5000)
+console.log(celular1)
