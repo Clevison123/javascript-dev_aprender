0c24a5128bb5b110cee9176f55cd37b0d5853145
Clevison123 <cdefensor467@gmail.com>
2023-04-18 14:12:23 -0300

    aulas

---
+++ aulas/math.js
@@ -0,0 +1,5 @@
+//Psr: Site Java Script Math
+
+//01 - Math.random()
+//02 - Math.Max(3,6,8,10)
+//03 - Math.Min(3,6,8,10)
---
+++ aulas/string.js
@@ -0,0 +1,15 @@
+//                  String
+//Psr: site JavaScript string
+
+//Tipo primitivo: const mensagem = 'Minha primeira mensagem'
+//mensagem.includes('primeira)
+//mensagem.startsWith('Minha')
+//mensagem.endsWith('mensagem')
+//mensagem.indexOf('primeira')
+//mensagem.replace('minha', 'sua')
+//mensagem.trim()
+//mensagem.split(' ')
+
+//Tipo objeto: const outraMensagem = new String('Bom dia!)
+//outraMensagem.length
+//outraMensagem.[2]
