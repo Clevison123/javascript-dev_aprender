1fc2bf01f90036b496120ed0773f23e13ebc1c24
Clevison123 <cdefensor467@gmail.com>
2023-04-18 16:51:56 -0300

    Template Literal

---
+++ aulas/template-literal.js
@@ -0,0 +1,25 @@
+//JavaScript - Template Literal
+//Psr:site javascript escape sequences
+
+//const mensagem = 'Olá! Está é minha \n primeira mensagem.' 
+//console.log(mensagem)
+
+//Object {}
+//Boolean true, false
+//string '' , ""
+//template ``
+
+const outra = `Olá! Está é minha 'segunda' mensagem.`
+console.log(outra)
+
+const nome = 'José'
+
+const email = 
+`Olá ${nome}
+
+obrigado pelo comentário
+
+assim que possível eu irei
+
+lhe responder.`
+console.log(email)
