905c38291e58163c187f8b3d4d58c7f1cc59382d
Clevison123 <cdefensor467@gmail.com>
2023-04-18 13:24:28 -0300

    constructor-functions

---
+++ aulas/constructor-functions.js
@@ -0,0 +1,35 @@
+function criarCelular(marcaCelular, tamanhoTela,capacidadeBateria){
+    return {
+        marcaCelular,
+        tamanhoTela,
+        capacidadeBateria,
+        ligar() {
+            console.log("Fazendo ligação...")
+        }
+    }
+}
+
+//camelCase - umDoisTresQuatro
+function criarCelular(marcaCelular,tamanhoTela, capacidadeBateria) {
+    return {
+        marcaCelular,
+        tamanhoTela,
+        capacidadeBateria,
+        ligar() {
+            console.log("Fazendo ligação...")
+        }
+    }
+}
+
+//Pascal Case - UmDoisTresQuatro
+function Celular(marcaCelular,tamanhoTela,capacidadeBateria){
+    this.marcaCelular = marcaCelular, 
+    this.tamanhoTela = tamanhoTela,
+    this.capacidadeBateria = capacidadeBateria,
+    this.ligar = function() {
+        console.log("Fazendo Ligação...")
+    }
+}
+
+const celular = new Celular('Asus', 5.5, 5000)
+console.log(celular)
