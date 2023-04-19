140c6d2847a6e11e6b45dc6e06075250527ec0f6
Clevison123 <cdefensor467@gmail.com>
2023-04-19 16:01:56 -0300

    JavaScript - Combinando um Array

---
+++ aulas/combinando-um-array.js
@@ -0,0 +1,13 @@
+//JavaScript - Combinando um Array
+
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+const combinado = numeros.join('.')
+console.log(combinado)
+
+const frase = "Olá Seja bem vindo ao curso"
+const resultado = frase.split(' ')
+console.log(resultado)
+
+console.log(resultado.join('-'))
+
