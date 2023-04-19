befd7865065e14b430d07bfb01ef9e95a2b35c33
Clevison123 <cdefensor467@gmail.com>
2023-04-19 13:25:25 -0300

    2 - Encontrar elementos

--- aulas/introducao-a-arrays002.js
+++ aulas/introducao-a-arrays002.js
@@ -7,3 +7,10 @@
 //6 - Combinar arrays
 
 //2 - Encontrar elementos(Primitivos)
+
+const numeros = [1,2,3,1,4]
+//console.log(numeros.indexOf(2))
+//console.log(numeros.lastIndexOf(1))
+//console.log(numeros.indexOf(2) !== -1)
+
+console.log(numeros.includes(2))
