59d4ca95b9bfd5dbdffd24bd5598dde847cb5d3a
Clevison123 <cdefensor467@gmail.com>
2023-04-19 19:03:15 -0300

    projeto segurança virtual

--- seguranca-virtual/app.js
+++ seguranca-virtual/app.js
@@ -3,3 +3,14 @@
 // Segundo - Eu preciso perguntar a ele(a) quem a convidou para essa festa
 // Terceiro - Eu vou criar um botão de verificação, para verificar se a pessoa que est-a tentando entrar foi de fato convidado por quem ela diz que a convidou
 //No final caso a pessoa foi realmente convidada ira aparecer uma frase 'Pode entrar! Você foi convidada.' caso contrario ira aparecer um frase 'Você não foi convidado'
+
+
+function verificar(){
+    NomeConvidado = document.getElementById('nome').value 
+    ConvidadosCristian = ['Jose', 'Victoria', 'Victor', 'Carol','Luis', 'Luisa']
+    if (ConvidadosCristian.includes(NomeConvidado)) {
+        document.getElementById('PermissaoDeEntrada').innerText = 'Você pode Entrar!'
+    } else {
+        document.getElementById('PermissaoDeEntrada').innerText = 'Você não pode Entrar!'
+    }
+}
