f041c98ee749cbd2388f8d529c500ac9f204603d
Clevison123 <cdefensor467@gmail.com>
2023-04-19 15:48:52 -0300

    JavaScript - Iterando um Array

---
+++ aulas/foreach.js
@@ -0,0 +1,10 @@
+//JavaScript - Iterando um Array
+
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+//for (numero of numeros)
+//        console.log(numero)
+
+
+//forEach
+numeros.forEach((numero, indice) => console.log(numero, indice))
