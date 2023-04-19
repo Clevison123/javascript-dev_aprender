c9e7f8737906e5c4122f2a02f46fd7c6b2e62e95
Clevison123 <cdefensor467@gmail.com>
2023-04-19 13:13:37 -0300

    introdução a arrays

---
+++ aulas/introducao-a-arrays002.js
@@ -0,0 +1,9 @@
+//JavaScript - Introdução a Arrays
+//1 - Add novos elementos
+//2 - Encontrar elementos
+//3 - Remover elementos
+//4 - Dividir elementos
+//5 - Dividir arrays
+//6 - Combinar arrays
+
+//2 - Encontrar elementos(Primitivos)
