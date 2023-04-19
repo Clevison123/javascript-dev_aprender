cb423fb5f531e7d4c82517ed1d08b6c933619b11
Clevison123 <cdefensor467@gmail.com>
2023-04-19 13:50:18 -0300

    Arrow Functions

---
+++ aulas/arrow-function.js
@@ -0,0 +1,10 @@
+//JavaScript - Arrow Functions
+
+const marcas = [
+    {id:1 , nome: 'a'},
+    {id:2 , nome: 'b'},
+    {id:3 , nome: 'c'}
+]
+
+console.log(marcas.find(marca =>  marca.nome === 'a'
+))
